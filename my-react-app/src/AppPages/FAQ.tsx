4ecb133152744045eba85c7f56ce50fad8e9abca
Belen Tesfaye <belentesfaye@Belens-MacBook-Pro.local>
2024-04-01 17:36:22 -0400

    changes by adding react app

---
+++ my-react-app/src/AppPages/FAQ.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React app.</p>
+    </div>
+  );
+}
+
+export default App;
