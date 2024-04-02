bbe5a2690e390f4a6b3b6ed15a4ac7bcb8cc5b63
Belen Tesfaye <tesfaye@northeastern.edu>
2024-04-02 16:38:56 -0400

    changes to the about and learn

--- frontend/src/components/MacBookPro141/MacBookPro141.tsx
+++ frontend/src/components/MacBookPro141/MacBookPro141.tsx
@@ -6,6 +6,31 @@ import { Group1Icon } from './Group1Icon';
 import classes from './MacBookPro141.module.css';
 import { Link } from 'react-router-dom';
 import { useCollapse } from 'react-collapsed';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Tab,
+  Table,
+  TableCaption,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  ToastId,
+  Tr,
+  useToast,
+} from '@chakra-ui/react';
 
 interface Props {
   className?: string;
@@ -41,26 +66,52 @@ function Section(props: SectionProps) {
   </div>
   );
 }
+const accountInfo = {
+  accountInfo: (
+    <div>
+      <Heading as='h3' size='md'>
+        {showLogin ? 'Login' : 'Sign Up'}
+      </Heading>
+      <FormControl>
+        <FormLabel htmlFor='name'>Username</FormLabel>
+        <Input
+          autoFocus
+          name='name'
+          placeholder='Your username'
+          value={userName}
+          onChange={event => setUserName(event.target.value)}
+        />
+      </FormControl>
+      <FormControl marginTop={4}>
+        <FormLabel htmlFor='password'>Password</FormLabel>
+        <Input
+          autoFocus
+          name='password'
+          placeholder='Your password'
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+      </FormControl>
+    </div>
+  ),
+};
+
 
 /* @figmaId 1:3 */
 export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) {
   const [isInfoBoxVisible, setInfoBoxVisible] = useState(false);
   const toggleInfoBox = () => setInfoBoxVisible(!isInfoBoxVisible);
-  const expandArrowStyle: CSSProperties = {
-    position: 'absolute',
-    left: '551px',
-    top: '41px',
-    width: '28px',
-    height: '19px',
-    backgroundImage: isInfoBoxVisible ? "url('assets/collapseArrow.png')" : "url('assets/expandArrow.png')",
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    cursor: 'pointer', // To indicate it's clickable
-  };
-
+  const [isAboutVisible, setAboutVisible] = useState(false);
+  const toggleAbout = () => setAboutVisible(!isAboutVisible);
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
+        {isInfoBoxVisible && (
+          <div className={classes.howItWorksResourcesHeritageWik}>
+            <div className={classes.textBlock}>How it works</div>
+            <div className={classes.textBlock2}>Resources</div>
+            <div className={classes.textBlock3}>HeritageWiki</div>
+          </div>
+        )}
       <div className={classes.heritage}>Heritage </div>
       <div className={classes.about}>About</div>
 
@@ -72,11 +123,13 @@ export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) 
       <div className={classes.line1}></div>
       <div className={classes.line2}></div>
       <div className={classes.line3}></div>
-      <div className={classes.expandArrow} onClick={toggleInfoBox}></div> 
+  
       <div className={classes.learn}>Learn</div>
+      <Link to="/sign-in" style={{ textDecoration: 'none' }}> 
       <div className={classes.signIn}>Sign In</div>
+      </Link>
       <div className={classes.help}>Help</div>
-      <div className={classes.expandArrow2}></div>
+      <div className={classes.expandArrow2} onClick={toggleInfoBox}></div>
       <div className={classes.stories}>Stories </div>
       <div className={classes.cultures}>Cultures</div>
       <div className={classes.communityForm}>Community Form</div>
@@ -92,13 +145,19 @@ export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) 
       <div className={classes.xboxR}></div>
       <div className={classes.chat}></div>
       <div className={classes.linkedIn}></div>
-      <div className={classes.expandArrow} onClick={toggleInfoBox}></div>
-        {isInfoBoxVisible && (
-          // Applying the .rectangle1 style to the info box
-          <div className={classes.rectangle1}>
+      <div className={classes.expandArrow} onClick={toggleAbout}></div>
+      {isInfoBoxVisible && (
+          <div className={classes.howItWorksResourcesHeritageWik}>
             <div className={classes.textBlock}>How it works</div>
             <div className={classes.textBlock2}>Resources</div>
             <div className={classes.textBlock3}>HeritageWiki</div>
+          </div>
+        )}
+      {isAboutVisible && (
+          // Applying the .rectangle1 style to the info box
+          <div className={classes.aboutBox}>
+            <div className={classes.textBlock2}>Our History</div>
+            <div className={classes.textBlock3}>Privacy</div>
           </div>
         )}
     </div>
