233237dd9584278c575de7d6212fe9f14bc70806
Belen Tesfaye <tesfaye@northeastern.edu>
2024-04-02 01:58:48 -0400

    added to expand arrow and changed up page outlook

---
+++ frontend/src/components/MacBookPro141/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+// Correct import statement
+import { useCollapse } from 'react-collapsed';
+
+import './MacBookPro141.module.css';
+
+function Section(props) {
+    const config = {
+        defaultExpanded: props.defaultExpanded || false,
+        collapsedHeight: props.collapsedHeight || 0
+    };
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
+
+    return (
+    <div className="collapsible">
+        <div className="header" {...getToggleProps()}>
+            <div className="title">{props.title}</div>
+            <div className="icon">
+                <i className={'fas fa-chevron-circle-' + (isExpanded ? 'up' : 'down')}></i>
+            </div>
+        </div>
+        <div {...getCollapseProps()}>
+            <div className="content">
+                {props.children}
+            </div>
+        </div>
+    </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="preferences">
+
+            <Section title="General" defaultExpanded="true">
+                <label>
+                    <input type="checkbox"/> Use dark theme
+                </label>
+                <label>
+                    <input type="checkbox"/> Use internet connection to download resources
+                </label>
+                <br/><br/>
+            </Section>
+
+            <Section title="Application Startup">
+                <label>
+                    <input type="checkbox"/> Open the application when you log into the computer
+                </label>
+                <br/><br/><br/><br/>
+            </Section>
+
+            <Section title="Notifications" collapsedHeight="32">
+                <i>Now you can get notifications to your smart watch!&nbsp;
+                    <a href="#">Learn more</a>
+                </i>
+                <br/><br/>
+                <label>
+                    <input type="checkbox"/> Notify me task statuses via SMS
+                </label>
+                <br/><br/>
+            </Section>
+
+        </div>
+    );
+}
+
+export default App;
--- frontend/src/components/MacBookPro141/MacBookPro141.tsx
+++ frontend/src/components/MacBookPro141/MacBookPro141.tsx
@@ -1,23 +1,69 @@
-import { memo, useState } from 'react'; // Import useState here
-import type { FC } from 'react';
+import React, { CSSProperties, memo, useState } from 'react';
+import type { FC, JSXElementConstructor, ReactElement, ReactNode, ReactPortal } from 'react';
 
 import resets from '../_resets.module.css';
 import { Group1Icon } from './Group1Icon';
 import classes from './MacBookPro141.module.css';
 import { Link } from 'react-router-dom';
+import { useCollapse } from 'react-collapsed';
 
 interface Props {
   className?: string;
 }
+interface SectionProps {
+  title: string;
+  defaultExpanded?: boolean;
+  collapsedHeight?: number;
+  children: React.ReactNode; // This type is appropriate for anything that can be rendered: numbers, strings, elements, or an array containing these types.
+}
+
+
+function Section(props: SectionProps) {
+  const config = {
+    defaultExpanded: props.defaultExpanded || false,
+    collapsedHeight: props.collapsedHeight || 0,
+  };
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
+
+  return (
+  <div className="collapsible">
+      <div className="header" {...getToggleProps()}>
+          <div className="title">{props.title}</div>
+          <div className="icon">
+              <i className={'fas fa-chevron-circle-' + (isExpanded ? 'up' : 'down')}></i>
+          </div>
+      </div>
+      <div {...getCollapseProps()}>
+          <div className="content">
+              {props.children}
+          </div>
+      </div>
+  </div>
+  );
+}
+
 /* @figmaId 1:3 */
 export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) {
   const [isInfoBoxVisible, setInfoBoxVisible] = useState(false);
   const toggleInfoBox = () => setInfoBoxVisible(!isInfoBoxVisible);
+  const expandArrowStyle: CSSProperties = {
+    position: 'absolute',
+    left: '551px',
+    top: '41px',
+    width: '28px',
+    height: '19px',
+    backgroundImage: isInfoBoxVisible ? "url('assets/collapseArrow.png')" : "url('assets/expandArrow.png')",
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    cursor: 'pointer', // To indicate it's clickable
+  };
 
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
       <div className={classes.heritage}>Heritage </div>
       <div className={classes.about}>About</div>
+
       <div className={classes.frame1}>
         <div className={classes.frame2}>
           <div className={classes.search}></div>
@@ -26,13 +72,7 @@ export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) 
       <div className={classes.line1}></div>
       <div className={classes.line2}></div>
       <div className={classes.line3}></div>
-      <div className={classes.expandArrow} onClick={toggleInfoBox}></div>
-      {isInfoBoxVisible && (
-        <div className={classes.infoBox}>
-          {/* Content of the information box */}
-          <p>Information box content goes here.</p>
-        </div>
-      )}
+      <div className={classes.expandArrow} onClick={toggleInfoBox}></div> 
       <div className={classes.learn}>Learn</div>
       <div className={classes.signIn}>Sign In</div>
       <div className={classes.help}>Help</div>
@@ -52,6 +92,15 @@ export const MacBookPro141: FC<Props> = memo(function MacBookPro141(props = {}) 
       <div className={classes.xboxR}></div>
       <div className={classes.chat}></div>
       <div className={classes.linkedIn}></div>
+      <div className={classes.expandArrow} onClick={toggleInfoBox}></div>
+        {isInfoBoxVisible && (
+          // Applying the .rectangle1 style to the info box
+          <div className={classes.rectangle1}>
+            <div className={classes.textBlock}>How it works</div>
+            <div className={classes.textBlock2}>Resources</div>
+            <div className={classes.textBlock3}>HeritageWiki</div>
+          </div>
+        )}
     </div>
   );
 });
